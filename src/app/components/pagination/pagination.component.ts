f3d80a9f72df9b08afcecfd465f8581aabc3d7ad
Acid-base <bart133@gmail.com>
2017-02-28 15:12:36 +0200

    change pagination

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router }            from '@angular/router';
+import { Location }          from '@angular/common';
 
 import { HttpService, PagerService } from '../../services/index';
 import { Post }                      from '../../shared/post.model';
@@ -15,18 +16,24 @@ export class PaginationComponent implements OnInit {
     pagedItems: any[];
     currentPage: number;
     constructor(
+        private location: Location,
         private httpService: HttpService,
         private pagerService: PagerService,
-        private router: Router) { }
-
+        private router: Router) {
+        location.subscribe(() => {
+            let path = this.location.path();
+            if (path.indexOf('page')) {
+                let page = parseFloat(path.slice(6));
+                this.setPage(page);
+            }
+        });
+    }
     ngOnInit() {
         this.getPosts();
-        this.listenEvents();
     }
     getPosts(): void {
         this.httpService.getPosts()
             .then(posts => {
-                // this.posts = posts;
                 this.posts = posts.reverse();
                 this.loadPage();
                 this.setPage(this.currentPage);
@@ -34,17 +41,8 @@ export class PaginationComponent implements OnInit {
     }
     loadPage(): void {
         if (localStorage.getItem('page')) {
-            this.currentPage = parseFloat(localStorage.getItem('page'));
-        }
-        if (window.location.hash.indexOf('page')) {
-        // if (window.location.pathname.indexOf('page')) {
-            let userPage = parseFloat(window.location.hash.slice(7));
-            // let userPage = parseFloat(window.location.pathname.slice(6));
-            this.pager = this.pagerService.getPager(this.posts.length, userPage);
-            if (userPage < 1 || userPage > this.pager.totalPages) {
-                this.router.navigate(['not-found']);
-            }
-            this.currentPage = userPage;
+            this.currentPage = JSON.parse(localStorage.getItem('page'));
+            this.router.navigate(['page/', this.currentPage]);
         }
     }
     setPage(page = 1): void {
@@ -53,12 +51,6 @@ export class PaginationComponent implements OnInit {
         }
         this.pager = this.pagerService.getPager(this.posts.length, page);
         this.pagedItems = this.posts.slice(this.pager.startIndex, this.pager.endIndex + 1);
-        localStorage.setItem('page', JSON.stringify(page));
-    }
-    listenEvents() {
-        window.addEventListener('popstate', () => {
-            this.loadPage();
-            this.setPage(this.currentPage);
-        });
+        localStorage.setItem('page', JSON.stringify(this.pager.currentPage));
     }
 }
