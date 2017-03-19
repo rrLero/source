a179a2646d90fc54bb71233efb4176b90a9441d6
Ihor Tkachuk <ihortkachuk@yandex.ua>
2017-03-19 13:56:57 +0200

    fixed access to service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,12 @@ import '../../public/css/styles.css';
 @Component({
     selector: 'app',
     templateUrl: 'app.component.html',
-    styleUrls: ['app.component.scss']
+    styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
     title = 'Blog platform';
     // githubUrl = 'https://github.com/login/oauth/authorize?client_id=caf9e03a36ecdaadcfb1&scope=repo&redirect_uri=http://localhost:8080/auth';
     githubUrl = 'https://github.com/login/oauth/authorize?client_id=48f5b894f42ae1f869d2&scope=repo&redirect_uri=http://acid.zzz.com.ua/auth';
 
-    constructor(private authService: AuthService) { }
+    constructor(public authService: AuthService) { }
 }
