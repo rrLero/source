eda931d1fdbda1df71955e457e9bdaa5db8a272d
Acid-base <bart133@gmail.com>
2017-04-01 16:44:39 +0300

    fix post-by-tag pagination

--- src/app/components/posts-by-tag/posts-by-tag.component.ts
+++ src/app/components/posts-by-tag/posts-by-tag.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit }                          from '@angular/core';
 import { Router, ActivatedRoute }                     from '@angular/router';
-// import { Location }                                   from '@angular/common';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
-import { HttpService, UserService, ToastService } from '../../services/index';
-
-import { Post } from '../../shared/post.model';
+import { HttpService, UserService, ToastService } from '../../services';
+import { Post }                                   from '../../shared/post.model';
 
 @Component({
     templateUrl: 'posts-by-tag.component.html',
@@ -25,31 +23,28 @@ import { Post } from '../../shared/post.model';
 })
 export class PostsByTagComponent implements OnInit {
     posts: Post[];
+    user: any;
+    tag: string;
+    paginationUrl: string;
     total = 0;
     perPage = 5;
+    empty = false;
+    canEdit = false;
     name = this.route.snapshot.params['name'];
     repo = this.route.snapshot.params['repo'];
-    tag: string;
     id = +this.route.snapshot.params['id'] || 1;
     url = `/${this.name}/${this.repo}`;
-    paginationUrl = `/${this.name}/${this.repo}/tag/${this.tag}`;
-    canEdit = false;
-    empty = false;
-    user: any;
     constructor(private router: Router,
-                // private location: Location,
                 private route: ActivatedRoute,
-                public toastService: ToastService,
                 private httpService: HttpService,
-                private userService: UserService) {
-                // location.subscribe(() => this.handlePageChange());
-    };
+                private userService: UserService,
+                public toastService: ToastService) { };
 
-    ngOnInit() {
+    ngOnInit(): void {
         this.getUser();
         this.getPosts();
     }
-    getUser() {
+    getUser(): void {
         this.user = this.userService.getUser();
         if (this.user) {
             this.userService
@@ -61,27 +56,22 @@ export class PostsByTagComponent implements OnInit {
     getPosts(id: number = this.id): void {
         this.route.params.forEach((param) => {
             this.tag = param.tag;
-            this.httpService
-                .getPostsByTag(this.name, this.repo, id, this.perPage, this.tag)
-                .then(res => {
-                    if (res.items) {
-                        this.posts = res.items;
-                        this.total = res.total;
-                        window.scrollTo(0, 0);
-                    } else {
-                        this.empty = true;
-                    }
-
-                })
-                .catch(error => this.toastService.showError(error));
+            this.paginationUrl = `${this.url}/tag/${this.tag}`;
         });
+        this.httpService
+            .getPostsByTag(this.name, this.repo, id, this.perPage, this.tag)
+            .then(res => {
+                if (res.items) {
+                    this.posts = res.items;
+                    this.total = res.total;
+                    window.scrollTo(0, 0);
+                } else {
+                    this.empty = true;
+                }
+            })
+            .catch(error => this.toastService.showError(error));
     }
     handlePageChange(page: number): void {
         this.getPosts(page);
-    }
-    savePage() {
-        // let page = this.location.path().split('/')[4] || '1';
-        let page = JSON.stringify(this.id);
-        localStorage.setItem('page', page);
     }
 }
